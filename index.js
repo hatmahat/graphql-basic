c693fe66cd5f705b6bf5fc1239d059a0f9819f97
hatmahat <mahatma.wisesa@sinarmasmining.com>
2023-11-29 18:21:23 +0700

    [LESSON-4] Schema & Types

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-// server setup
+// types
+import { typeDefs } from "./schema";
+
+ // server setup
 const server = new ApolloServer({
-    // typeDefs
-    // resolvers
-}) 
+  typeDefs, // -- defenitions of types of data
+  // resolvers // -- contains a bunch of resolver functions (to handle the queries based on our schema)
+});
 
-const {url} = await startStandaloneServer(server, {
-    listen: {port: 4000}
-})
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-console.log('Server ready at port', 4000);
+console.log("Server ready at port", 4000);
